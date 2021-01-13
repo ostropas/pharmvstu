ce526a89c52db5f119a213ed9be5f8e11eba8b6b
Pavel Ostroukhov <p.ostroukhov@doudougames.ru>
2021-01-13 22:02:04 +0300

    Added logging

---
+++ PharmacyVSTU/Logger/PharmLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace PharmacyVSTU.Logger
+{
+    public class PharmLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new PharmLogger();
+        }
+ 
+        public void Dispose() { }
+ 
+        private class PharmLogger : ILogger
+        {
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+ 
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+ 
+            public void Log<TState>(LogLevel logLevel, EventId eventId, 
+                TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                var currentTime = DateTime.UtcNow.ToString();
+                var cFormatter = logLevel == LogLevel.Error || logLevel == LogLevel.Critical ? ErrorFormat :
+                formatter; 
+                File.AppendAllText("pharmacy.log", $"Time:[{currentTime}] Level:[{logLevel}] Log:[{cFormatter(state, exception)}]\n");
+                Console.WriteLine(cFormatter(state, exception));
+            }
+
+            private string ErrorFormat<TState>(TState state, Exception e)
+            {
+                return $"{state} {e}";
+            }
+        }
+    }
+}
--- PharmacyVSTU/Program.cs
+++ PharmacyVSTU/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PharmacyVSTU.Logger;
 
 namespace PharmacyVSTU
 {
@@ -13,7 +14,10 @@ namespace PharmacyVSTU
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).ConfigureLogging(builder =>
+            {
+                builder.ClearProviders().AddProvider(new PharmLoggerProvider());
+            }).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
