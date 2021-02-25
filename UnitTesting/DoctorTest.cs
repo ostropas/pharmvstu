5261abb641cc3ca18e3ba4e4aafec85847aa09a6
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2021-02-26 00:50:06 +0300

    Change tests

---
+++ UnitTesting/DoctorTest.cs
@@ -0,0 +1,16 @@
+using System;
+using PharmacyVSTU.Controllers;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class DoctorTest
+    {
+        [Fact]
+        public void DoctorInfo()
+        {
+            var controller = new DoctorController(null, null);
+            Assert.Equal(controller.GetCurrentDoctorInfo(), "It works");
+        }
+    }
+}
