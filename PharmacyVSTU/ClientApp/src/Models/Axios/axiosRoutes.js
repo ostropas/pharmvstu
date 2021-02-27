cb877d4ff346322b3973c7ba33197d9ecd9d167d
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2021-02-27 12:40:14 +0300

    Added user profile page

--- PharmacyVSTU/ClientApp/src/Models/Axios/axiosRoutes.js
+++ PharmacyVSTU/ClientApp/src/Models/Axios/axiosRoutes.js
@@ -1,19 +1,88 @@
 import axios from "../Axios/axios.js"
 
+class DevAxios
+{
+    post(url, data, expetedResponse) {
+        console.log("POST")
+        console.log("Url: " + url);
+        console.log("Data: " + JSON.stringify(data));
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let res = {data:expetedResponse}
+                resolve(res)
+            }, 2000);            
+        })
+    }
+
+    get(url, expetedResponse) {
+        console.log("GET")
+        console.log("Url: " + url);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let res = {data:expetedResponse}
+                resolve(res)
+            }, 2000);            
+        })
+    }
+
+    put(url, data, expetedResponse) {
+        console.log("PUT")
+        console.log("Url: " + url);
+        console.log("Data: " + JSON.stringify(data));
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let res = {data:expetedResponse}
+                resolve(res)
+            }, 2000);            
+        })
+    }
+}
+
 class Routes {
+    sender = process.env.NODE_ENV === 'development' ? new DevAxios() : axios;
+
     signIn(email, pass)
     {
-        return axios.post("/auth", {email: email, password: pass})
+        return this.sender.post("/auth", {email: email, password: pass}, "jwttoken")
     }
 
-    signUp(email, pass, fio)
+    signUp(email, pass, fio, isDoctor)
     {
-        return axios.post("/reg", {email: email, password: pass, fio: fio})
+        return this.sender.post("/reg", {email: email, password: pass, fio: fio, doctor:isDoctor}, "jwttoken")
     }
 
-    test()
+    getUserData()
     {
-        return axios.get("/api/doctor/data");
+        return this.sender.get("/user", {
+            "id": 1,
+            "email": "test@test.ru",
+            "fio": "Иванов Иван Иванович",
+            "doctor": true
+          })
+    }
+
+    getDoctorData(doctorId)
+    {
+        return this.sender.get(`/doctor/info?doctorId=${doctorId}`, {
+            "doctorId": 1,
+            "fio": "Иванов Иван Иванович",
+            "info": "Такой крутой парень"
+          })
+    }
+
+    updatePatientData(data)
+    {
+        return this.sender.put("/patient/info", data, {
+            "succsess": true
+          })
+    }
+
+
+    updateDoctorData(data)
+    {
+        return this.sender.put("/doctor/info", data, {
+            "succsess": true
+          })
     }
 }
 
